171c87f949063b9acb26f0ec71f21187275eb395
BurgerMinus <burgerminus@gmail.com>
2025-11-30 00:08:11 -0700

    enemies now affected by Event Horizon gravity well

--- extensions/Scripts/Hosts/ShieldBot/ShieldBot.gd
+++ extensions/Scripts/Hosts/ShieldBot/ShieldBot.gd
@@ -1,15 +1,21 @@
 extends "res://Scripts/Hosts/ShieldBot/ShieldBot.gd"
 
 var gravity_tackle = false
+var base_base_shield_enemy_repulsion = 1500
+var GRAVITY_STRENGTH = 3.0
 
 func toggle_enhancement(state):
 	gravity_tackle = false
+	base_base_shield_enemy_repulsion = 1500
 	super(state)
 	gravity_tackle = get_currently_applicable_upgrades()['event_horizon'] > 0
+	base_base_shield_enemy_repulsion = base_shield_enemy_repulsion
 
 func start_tackle():
 	super()
 	if gravity_tackle:
+		invincible = true
+		base_shield_enemy_repulsion = -base_base_shield_enemy_repulsion * GRAVITY_STRENGTH
 		set_shield_width(2*PI)
 		set_shield_color(Vector3(0.33, 0, 0.5))
 		set_shield_active(true)
@@ -17,6 +23,8 @@ func start_tackle():
 func end_tackle():
 	super()
 	if gravity_tackle:
+		invincible = false
+		base_shield_enemy_repulsion = base_base_shield_enemy_repulsion
 		update_overburden_penalties() # reset shield width and color
 
 func update_overburden_penalties():
